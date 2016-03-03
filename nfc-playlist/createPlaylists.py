133bcbfce34ba1caad838c3e954b625179d0da07
wingelss <sascha.wingels@herzotec.de>
2016-03-03 21:34:31 +0100

    no message

--- nfc-playlist/createPlaylists.py
+++ nfc-playlist/createPlaylists.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import os
 import fnmatch
 from mutagen.easyid3 import EasyID3
 
 # create playlists
-mediaDir = '/music/Network/mp3'
+mediaDir = '/var/lib/mopidy/media/mp3'
 playlistsDir = '/var/lib/mopidy/playlists'
 
 for root, dirs, files in os.walk(mediaDir):
@@ -13,7 +14,6 @@ for root, dirs, files in os.walk(mediaDir):
         # grep all mp3 files
         matches = fnmatch.filter(files, '*.mp3')
         if len(matches) > 0:
-            logger.info(matches[0])
             audio = EasyID3(os.path.join(root, matches[0]))
             with open(os.path.join(playlistsDir, audio['artist'][0] + ' - ' + audio['album'][0] + '.m3u'), 'wb') as m3u:
                 for match in matches:
